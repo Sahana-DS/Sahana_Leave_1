550ad561a85374bfe436d619ccf24df6fab5a56c
Sahana-DS <sahana2000ds@gmail.com>
2025-07-02 12:23:14 +0530

    sahana_leave_1

--- Backend/Server.js
+++ Backend/Server.js
@@ -259,5 +259,5 @@ app.put('/api/leave-requests/:id', async (req, res) => {
 
 app.listen(port, async () => {
   await initializeDatabase();
-  console.log(`Server running at http://13.60.82.52:${port}`);
+  console.log(`Server running at http://56.228.41.213:${port}`);
 });
